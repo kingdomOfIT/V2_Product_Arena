a525dbd51b3af73deb0c361c0c41cf6e0a90c6d2
Er0rx <xer0rx123@gmail.com>
2023-02-10 15:04:35 +0100

    Finalization

--- lib/web/reusable_web_widgets/loader.dart
+++ lib/web/reusable_web_widgets/loader.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class RedirectingScreen extends StatefulWidget {
+  static const routeName = '/redirection';
   const RedirectingScreen({super.key});
 
   @override
