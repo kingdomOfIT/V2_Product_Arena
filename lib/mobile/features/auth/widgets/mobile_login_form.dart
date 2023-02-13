1a23fef5d11d27e3b74e69003ce803135ac4ae64
bkaric <bkaric@pa.tech387.com>
2023-02-13 16:37:55 +0100

    Mobile_login_form - added final keyword

--- lib/mobile/features/auth/widgets/mobile_login_form.dart
+++ lib/mobile/features/auth/widgets/mobile_login_form.dart
@@ -19,7 +19,7 @@ class LoginForm extends StatelessWidget {
     required this.togglePasswordView,
   });
 
-  RegExp email = RegExp(
+  final RegExp email = RegExp(
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
 
   @override
