319b7b827279eddf76dfe07e81d586241d028b68
bkaric <bkaric@pa.tech387.com>
2023-02-13 14:09:50 +0100

    Integration tests - web and mobile signup flow

--- integration_test/mobile_app_test.dart
+++ integration_test/mobile_app_test.dart
@@ -6,25 +6,60 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:v2_product_arena/amplifyconfiguration.dart';
 import 'package:v2_product_arena/mobile/features/auth/screens/email_verification_screen.dart';
-import 'package:v2_product_arena/mobile/features/auth/screens/mobile_login_screen.dart';
+import 'package:v2_product_arena/mobile/features/auth/screens/email_verified_screen.dart';
 import 'package:v2_product_arena/mobile/features/auth/screens/mobile_signup_screen.dart';
+import 'package:v2_product_arena/mobile/features/onboarding/screens/mobile_onboarding_screen.dart';
 import 'package:v2_product_arena/mobile/providers/mobile_auth_provider.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'mobile_auth_provider_test.mocks.dart';
 
-late MobileAuth auth;
+class MockMobileAuth extends MobileAuth {}
+
+class MockBuildContext extends Mock implements BuildContext {}
+
+class MockAuth extends Mock implements AuthCategory {
+  @override
+  Future<SignUpResult> signUp(
+      {required String username,
+      required String password,
+      SignUpOptions? options}) async {
+    var result = MockSignUpResult();
+    when(result.isSignUpComplete).thenReturn(true);
+    return result;
+  }
+
+  @override
+  Future<SignUpResult> confirmSignUp(
+      {required String username,
+      required String confirmationCode,
+      ConfirmSignUpOptions? options}) async {
+    var result = MockSignUpResult();
+    when(result.isSignUpComplete).thenReturn(true);
+    return result;
+  }
+}
+
+late MobileAuth mobileAuthent;
 
 Widget createMobileSignupScreen() => ChangeNotifierProvider<MobileAuth>(
       create: (context) {
-        auth = MobileAuth();
-        return auth;
+        mobileAuthent = MobileAuth();
+        return mobileAuthent;
       },
       child: MaterialApp(
         routes: {
-          '/mobile-login': (context) => const MobileLoginScreen(),
+          '/email-verification': (context) => const EmailVerificationScreen(),
+          '/email-verified': (context) => const EmailVerified(),
+          '/mobile-onboarding': (context) => const MobileOnboardingScreen(),
         },
         home: const MobileSignupScreen(),
       ),
     );
+
+@GenerateMocks([SignUpResult, AmplifyClass])
 void main() {
+  MockBuildContext mockContext = MockBuildContext();
   group('Testing App Performance Tests', () {
     IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
@@ -40,51 +75,110 @@ void main() {
       }
     });
     testWidgets('Mobile signup', (tester) async {
+      MobileAuth mobileAuth = MockMobileAuth();
+      MockAmplifyClass test = MockAmplifyClass();
+      when(test.Auth).thenReturn(MockAuth());
+      AmplifyClass.instance = test;
       await tester.pumpWidget(createMobileSignupScreen());
       await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('nameTextField')));
       await tester.enterText(find.byKey(const Key('nameTextField')), 'Test');
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('surnameTextField')));
       await tester.enterText(find.byKey(const Key('surnameTextField')), 'Test');
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('birthDateTextField')));
       await tester.enterText(
           find.byKey(const Key('birthDateTextField')), '01-01-1997');
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('cityTextField')));
       await tester.enterText(
           find.byKey(const Key('cityTextField')), 'Sarajevo');
-
-      await tester.tap(find.byKey(const Key('dropdownButton')));
       await tester.pumpAndSettle();
 
+      await tester.tap(find.byKey(const Key('dropdownButton')));
+      await tester.pump(const Duration(seconds: 1));
       final dropdownItem = find.text('Student').last;
-
       await tester.tap(dropdownItem);
       await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('phoneTextField')));
+      await tester.pumpAndSettle();
       await tester.enterText(
-          find.byKey(const Key('phoneTextField')), '+387123456789');
+          find.byKey(const Key('phoneTextField')), '123456789');
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('emailTextField')));
       await tester.enterText(
           find.byKey(const Key('emailTextField')), 'testing@gmail.com');
+      await tester.pumpAndSettle();
 
       await tester.ensureVisible(find.byKey(const Key('passwordTextField')));
-
       await tester.tap(find.byKey(const Key('passwordTextField')));
       await tester.enterText(
           find.byKey(const Key('passwordTextField')), 'Testing1!');
-
       await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('signupButton')));
       await tester.pumpAndSettle();
 
-      expect(find.byType(EmailVerificationScreen), findsOneWidget);
+      await mobileAuth.signUpUser(
+          "Test",
+          "Test",
+          "01-01-1997",
+          "Sarajevo",
+          "Student",
+          "123456789",
+          "testing@gmail.com",
+          "Testing1!",
+          "/email-verification");
+
+      await tester.tap(find.byKey(const Key('emailverificationField1')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField1')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverificationField2')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField2')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverificationField3')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField3')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverificationField4')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField4')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverificationField5')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField5')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverificationField6')));
+      await tester.enterText(
+          find.byKey(const Key('emailverificationField6')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('emailverifyButton')));
+      await tester.pumpAndSettle();
+
+      await mobileAuth.confirmUser(
+          'testing@gmail.com', '111111', mockContext, '/email-verified');
+      await tester.pumpAndSettle();
     });
   });
 }
--- integration_test/web_app_test.dart
+++ integration_test/web_app_test.dart
@@ -1,60 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:provider/provider.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:v2_product_arena/amplifyconfiguration.dart';
-import 'package:v2_product_arena/web/features/auth/screens/web_login_screen.dart';
+import 'package:mockito/mockito.dart';
+import 'package:v2_product_arena/web/features/auth/screens/web_email_verifed.dart';
 import 'package:v2_product_arena/web/features/auth/screens/web_signup_screen.dart';
 import 'package:v2_product_arena/web/features/auth/screens/web_verification_screen.dart';
+import 'package:v2_product_arena/web/features/onboarding/screens/web_onboarding_screen.dart';
 import 'package:v2_product_arena/web/providers/web_auth_provider.dart';
+import 'mobile_auth_provider_test.mocks.dart';
 
-late WebAuth webAuth;
+class MockWebAuth extends WebAuth {}
+
+class MockBuildContext extends Mock implements BuildContext {}
+
+class MockAuth extends Mock implements AuthCategory {
+  @override
+  Future<SignUpResult> signUp(
+      {required String username,
+      required String password,
+      SignUpOptions? options}) async {
+    var result = MockSignUpResult();
+    when(result.isSignUpComplete).thenReturn(true);
+    return result;
+  }
+
+  @override
+  Future<SignUpResult> confirmSignUp(
+      {required String username,
+      required String confirmationCode,
+      ConfirmSignUpOptions? options}) async {
+    var result = MockSignUpResult();
+    when(result.isSignUpComplete).thenReturn(true);
+    return result;
+  }
+}
+
+late WebAuth webAuthent;
 
 Widget createWebSignUpScreen() => ChangeNotifierProvider<WebAuth>(
       create: (context) {
-        webAuth = WebAuth();
-        return webAuth;
+        webAuthent = WebAuth();
+        return webAuthent;
       },
       child: MaterialApp(
         routes: {
-          '/web-login': (context) => const WebLoginScreen(),
+          '/confirmation': (context) => SignupConfirmation(),
+          '/verified': (context) => Verifed(),
+          '/web-onboarding': (context) => const WebOnboardingScreen(),
         },
         home: WebSignUpScreen(),
       ),
     );
+
+@GenerateMocks([SignUpResult, AmplifyClass])
 void main() {
+  MockBuildContext mockContext = MockBuildContext();
   group('Testing App Performance Tests', () {
     IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-    test('can configure Amplify', () async {
-      AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
-      await Amplify.addPlugin(authPlugin);
-
-      try {
-        await Amplify.configure(amplifyconfig);
-      } on AmplifyAlreadyConfiguredException {
-        safePrint(
-            "Tried to reconfigure Amplify: This can occur when your app restarts on Android.");
-      }
-    });
     testWidgets('Web signup', (tester) async {
+      WebAuth webAuth = MockWebAuth();
+      MockAmplifyClass test = MockAmplifyClass();
+      when(test.Auth).thenReturn(MockAuth());
+      AmplifyClass.instance = test;
       await tester.pumpWidget(createWebSignUpScreen());
       await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('nameSignup')));
       await tester.enterText(find.byKey(const Key('nameSignup')), 'Test');
 
+      await tester.pumpAndSettle();
+
       await tester.tap(find.byKey(const Key('surnameSignup')));
       await tester.enterText(find.byKey(const Key('surnameSignup')), 'Test');
+
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('birthdateSignup')));
       await tester.enterText(
           find.byKey(const Key('birthdateSignup')), '01-01-1997');
 
+      await tester.pumpAndSettle();
+
       await tester.tap(find.byKey(const Key('citySignup')));
       await tester.enterText(find.byKey(const Key('citySignup')), 'Sarajevo');
+
+      await tester.pumpAndSettle();
 
       await tester.tap(find.byKey(const Key('dropdownButtonSignup')));
       await tester.pumpAndSettle();
@@ -68,9 +101,13 @@ void main() {
       await tester.enterText(
           find.byKey(const Key('phoneSignup')), '+387123456789');
 
+      await tester.pumpAndSettle();
+
       await tester.tap(find.byKey(const Key('emailSignup')));
       await tester.enterText(
           find.byKey(const Key('emailSignup')), 'testing@gmail.com');
+
+      await tester.pumpAndSettle();
 
       await tester.ensureVisible(find.byKey(const Key('passwordSignup')));
 
@@ -80,10 +117,52 @@ void main() {
 
       await tester.pumpAndSettle();
 
+      await tester.ensureVisible(find.byKey(const Key('signupButton')));
       await tester.tap(find.byKey(const Key('signupButton')));
       await tester.pumpAndSettle();
 
-      expect(find.byType(SignupConfirmation), findsOneWidget);
+      await webAuth.signUpUser("name", "surname", "birthDate", "city", "status",
+          "phone", "email", "password", mockContext, "/confirmation");
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField1')));
+      await tester.enterText(find.byKey(const Key('verificationField1')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField2')));
+      await tester.enterText(find.byKey(const Key('verificationField2')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField3')));
+      await tester.enterText(find.byKey(const Key('verificationField3')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField4')));
+      await tester.enterText(find.byKey(const Key('verificationField4')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField5')));
+      await tester.enterText(find.byKey(const Key('verificationField5')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verificationField6')));
+      await tester.enterText(find.byKey(const Key('verificationField6')), '1');
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('verifyButton')));
+      await tester.pumpAndSettle();
+
+      await webAuth.confirmUser(
+          'testing@gmail.com', '111111', mockContext, '/verified');
+
+      await tester.pumpAndSettle();
     });
   });
 }
