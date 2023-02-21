cd3c2d10aacdc50dc370a9ae6d05f7a899678353
Er0rx <xer0rx123@gmail.com>
2023-02-21 13:53:13 +0100

    Reusable sidebar, homescreen, contact page, active navigation

---
+++ lib/web/reusable_web_widgets/web_homepage_sidebar.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:v2_product_arena/web/features/home/screens/web_contact_screen.dart';
+import 'package:v2_product_arena/web/features/home/screens/web_home_screen.dart';
+
+enum _CurrentPage {
+  home,
+  design,
+  recent,
+  contact,
+}
+
+var currentPage = _CurrentPage.home;
+
+class WebHomeSideBar extends StatefulWidget {
+  static const routeName = '/web-home';
+
+  const WebHomeSideBar({super.key});
+
+  @override
+  State<WebHomeSideBar> createState() => _WebHomeSideBarState();
+}
+
+class _WebHomeSideBarState extends State<WebHomeSideBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //width: 310,
+      color: Colors.black,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Image.asset("assets/images/LogoPAblackbackground.png"),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  Icons.home,
+                  color: currentPage == _CurrentPage.home
+                      ? const Color(0xFF22E974)
+                      : Colors.white,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Homescreen',
+                  style: GoogleFonts.notoSans(
+                    color: currentPage == _CurrentPage.home
+                        ? const Color(0xFF22E974)
+                        : Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() {
+                currentPage = _CurrentPage.home;
+              });
+              Navigator.of(context)
+                  .pushReplacementNamed(WebHomeScreen.routeName);
+            },
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  Icons.auto_fix_high,
+                  color: currentPage == _CurrentPage.design
+                      ? const Color(0xFF22E974)
+                      : Colors.white,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'UI/UX Design',
+                  style: GoogleFonts.notoSans(
+                    color: currentPage == _CurrentPage.design
+                        ? const Color(0xFF22E974)
+                        : Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() {
+                currentPage = _CurrentPage.design;
+              });
+            },
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  Icons.bookmark,
+                  color: currentPage == _CurrentPage.recent
+                      ? const Color(0xFF22E974)
+                      : Colors.white,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Recent lectures',
+                  style: GoogleFonts.notoSans(
+                    color: currentPage == _CurrentPage.recent
+                        ? const Color(0xFF22E974)
+                        : Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() {
+                currentPage = _CurrentPage.recent;
+              });
+            },
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  Icons.contact_page,
+                  color: currentPage == _CurrentPage.contact
+                      ? const Color(0xFF22E974)
+                      : Colors.white,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Contact us!',
+                  style: GoogleFonts.notoSans(
+                    color: currentPage == _CurrentPage.contact
+                        ? const Color(0xFF22E974)
+                        : Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() {
+                currentPage = _CurrentPage.contact;
+              });
+              Navigator.of(context)
+                  .pushReplacementNamed(WebContactScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
