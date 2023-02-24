46278e1d1a71df2e564e91d6e0cad3f2e01376a7
bkaric <bkaric@pa.tech387.com>
2023-02-24 14:46:00 +0100

    Removed conflict errors from git merge

--- lib/web/features/home/screens/web_lectures_screen.dart
+++ lib/web/features/home/screens/web_lectures_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:v2_product_arena/web/features/home/models/lecture.dart';
 import 'package:v2_product_arena/web/providers/web_auth_provider.dart';
-import 'package:v2_product_arena/web/providers/web_auth_provider.dart';
 import 'package:v2_product_arena/web/reusable_web_widgets/web_lecture_card.dart';
 
 import '../../../../constants/global_variables.dart';
@@ -24,7 +23,6 @@ class _WebLecturesPageState extends State<WebLecturesPage> {
   @override
   Widget build(BuildContext context) {
     double maxwidth = MediaQuery.of(context).size.width;
-    final lectures = Provider.of<WebAuth>(context, listen: false).lectures;
     final lectures = Provider.of<WebAuth>(context, listen: false).lectures;
     return Scaffold(
       body: Row(
