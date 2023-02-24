78182e7c61b1f371df11615f707c44a79b8eb081
bkaric <bkaric@pa.tech387.com>
2023-02-24 14:07:40 +0100

    Welcome lectures screen test added

---
+++ test/mobile/features/lectures/screens/welcome_lectures_screen_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:v2_product_arena/mobile/features/lectures/screens/welcome_lectures_screen.dart';
+import 'package:v2_product_arena/mobile/providers/mobile_auth_provider.dart';
+
+late MobileAuth auth;
+
+Widget createMobileWelcomeLecturesScreen() =>
+    ChangeNotifierProvider<MobileAuth>(
+      create: (context) {
+        auth = MobileAuth();
+        return auth;
+      },
+      child: const MaterialApp(
+        home: WelcomeLecturesScreen(),
+      ),
+    );
+void main() {
+  testWidgets('welcome lectures screen ...', (tester) async {
+    await tester.pumpWidget(createMobileWelcomeLecturesScreen());
+    await tester.pumpAndSettle();
+    final button = find.text('Next');
+    await tester.tap(button);
+    await tester.pumpAndSettle();
+  });
+}
