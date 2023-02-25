21828d053784e64a9a705f0e4836d3f118be0da8
Asmir Alic <asmir.exel@hotmail.com>
2023-02-25 19:49:13 +0100

    Home Page fixing

--- lib/web/features/home/screens/web_home_screen.dart
+++ lib/web/features/home/screens/web_home_screen.dart
@@ -12,114 +12,72 @@ class WebHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double maxwidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       body: Row(
         children: [
           const WebSideBar(),
-          SizedBox(
+          Container(
+            color: Colors.white,
             width: maxwidth * (1130 / 1440),
             child: Column(
               children: [
                 const GreenProfileIcon(),
-                Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            (MediaQuery.of(context).size.width > 920)
-                                ? Image.asset(
-                                    'assets/images/TopUI.png',
-                                    //height: 200,
-                                    width: MediaQuery.of(context).size.width *
-                                        0.15,
-                                  )
-                                : const SizedBox(),
-                            (MediaQuery.of(context).size.width > 920)
-                                ? const SizedBox(
-                                    width: 50,
-                                  )
-                                : const SizedBox(),
-                            (MediaQuery.of(context).size.width > 920)
-                                ? RichText(
-                                    text: TextSpan(
-                                      text: 'Welcome to\n',
-                                      style: GoogleFonts.notoSans(
-                                        fontSize: 40,
-                                      ),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Product Arena',
-                                          style: GoogleFonts.notoSans(
-                                            fontSize: 40,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                : Column(
-                                    children: [
-                                      Text(
-                                        'Welcome to',
-                                        style: GoogleFonts.notoSans(
-                                          fontSize: maxWidth * (60 / 1440),
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                      Text(
-                                        'Product Arena',
-                                        style: GoogleFonts.notoSans(
-                                          fontSize: maxWidth * (60 / 1440),
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                'Our goal is to recognise persistence, motivation and\nadaptability,\nthat’s why we encourage you to dive into these materials\nand wish you the best of luck in your studies.',
-                                style: GoogleFonts.outfit(
-                                  fontSize: maxWidth * (32 / 1440),
-                                  fontWeight: FontWeight.w400,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 68),
-                              Text(
-                                'Once you have gone through all the lessons you’ll be able to take\na test to show us what you have learned!',
-                                style: GoogleFonts.outfit(
-                                  fontSize: maxWidth * (32 / 1440),
-                                  fontWeight: FontWeight.w400,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 58.93),
-                              Align(
-                                alignment: Alignment.bottomRight,
-                                child: SizedBox(
-                                  width: maxWidth * (250 / 1440),
-                                  child:
-                                      Image.asset('assets/images/BottomQA.png'),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    SizedBox(
+                      width: maxwidth * (250 / 1440),
+                      child: Image.asset('assets/images/TopUI.png'),
                     ),
+                    SizedBox(
+                      width: maxwidth * (90 / 1440),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome to',
+                          style: GoogleFonts.notoSans(
+                              fontSize: maxwidth * (60 / 1440),
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          'Product Arena',
+                          style: GoogleFonts.notoSans(
+                              fontSize: maxwidth * (60 / 1440),
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  'Our goal is to recognise persistence, motivation and adaptability, that’s why we encourage you to dive into these materials and wish you the best of luck in your studies.',
+                  style: GoogleFonts.outfit(
+                    fontSize: maxwidth * (32 / 1440),
+                    fontWeight: FontWeight.w400,
                   ),
-                ],
-              ),
-              const Positioned(
-                child: WebProfilePopup(),
-              ),
-            ],
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Once you have gone through all the lessons you’ll be able to take a test to show us what you have learned!',
+                  style: GoogleFonts.outfit(
+                    fontSize: maxwidth * (32 / 1440),
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: SizedBox(
+                    width: maxwidth * (250 / 1440),
+                    child: Image.asset('assets/images/BottomQA.png'),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
