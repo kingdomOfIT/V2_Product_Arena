bbef415e49c435e0139d79c5c5a750821f435ab4
Asmir Alic <asmir.exel@hotmail.com>
2023-02-25 21:08:34 +0100

    Home Page idiotic solution

--- lib/web/features/home/screens/web_home_screen.dart
+++ lib/web/features/home/screens/web_home_screen.dart
@@ -16,125 +16,22 @@ class WebHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double maxwidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Row(
         children: [
           const WebSideBar(),
           Container(
-            color: Colors.white,
-            width: maxwidth * (1130 / 1440),
-            child: Column(
-              children: [
-                const GreenProfileIcon(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: maxwidth * (250 / 1440),
-                      child: Image.asset('assets/images/TopUI.png'),
-                    ),
-                    SizedBox(
-                      width: maxwidth * (90 / 1440),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            (MediaQuery.of(context).size.width > 920 &&
-                                    MediaQuery.of(context).size.height > 790)
-                                ? Image.asset(
-                                    'assets/images/TopUI.png',
-                                    //height: 200,
-                                    width: MediaQuery.of(context).size.width *
-                                        0.15,
-                                  )
-                                : const SizedBox(),
-                            (MediaQuery.of(context).size.width > 920 &&
-                                    MediaQuery.of(context).size.height > 790)
-                                ? const SizedBox(
-                                    width: 50,
-                                  )
-                                : const SizedBox(),
-                            (MediaQuery.of(context).size.width > 920)
-                                ? RichText(
-                                    text: TextSpan(
-                                      text: 'Welcome to\n',
-                                      style: GoogleFonts.notoSans(
-                                        fontSize: 40,
-                                      ),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Product Arena',
-                                          style: GoogleFonts.notoSans(
-                                            fontSize: 40,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                : Column(
-                                    children: [
-                                      Text(
-                                        'Welcome to',
-                                        style: GoogleFonts.notoSans(
-                                          fontSize: 40,
-                                        ),
-                                      ),
-                                      Text(
-                                        'Product Arena',
-                                        style: GoogleFonts.notoSans(
-                                          fontSize: 40,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.45,
-                          child: Text(
-                            'Our goal is to recognise persistence, motivation and adaptability, that’s why we encourage you to dive into these materials and wish you the best of luck in your studies.',
-                            style: GoogleFonts.outfit(
-                              fontSize: 26,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.45,
-                          child: Text(
-                            "Once you have gone through all the lessons you'll be able to take a test to show us what you have learned!",
-                            style: GoogleFonts.outfit(
-                              fontSize: 26,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
-                    (MediaQuery.of(context).size.width > 920 &&
-                            MediaQuery.of(context).size.height > 790)
-                        ? Image.asset(
-                            'assets/images/BottomQA.png',
-                            height: 200,
-                            width: MediaQuery.of(context).size.width * 0.5,
-                            alignment: Alignment.centerRight,
-                          )
-                        : const SizedBox(),
-                  ],
-                ),
-              ],
+            alignment: Alignment.center,
+            width: maxwidth * (900 / 1440),
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/Homescreenbody.png'),
+                  fit: BoxFit.cover),
             ),
+            child: const Text(''),
           ),
+          const GreenProfileIcon()
         ],
       ),
     );
